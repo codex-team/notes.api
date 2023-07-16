f7cb67457d8c49be11bb418fc71313f0c19c0176
slaveeks <chernyshev2001@icloud.com>
2023-07-16 23:39:19 +0300

    Installed node-fetch, added base for fetch.transport

---
+++ src/repository/transport/fetch.transport.ts
@@ -0,0 +1,29 @@
+import fetch from 'node-fetch';
+
+/**
+ * Fetch transport to make HTTP requests
+ */
+export default class FetchTransport {
+  /**
+   * Fetch constructor
+   *
+   * @param baseUrl - Base URL
+   */
+  constructor(private readonly baseUrl: string) {
+  }
+
+  /**
+   * Make GET request
+   *
+   * @template Response - Response data type
+   * @param endpoint - API endpoint
+   * @returns { Promise<Response> } - Response data
+   */
+  public async get<Response>(endpoint: string): Promise<Response> {
+    const response = await fetch(this.baseUrl + endpoint, {
+      method: 'GET',
+    });
+
+    return await response.json() as Response;
+  }
+}
