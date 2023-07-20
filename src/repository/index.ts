df523ffac65ee841ea24a4c655385cbfaeac91bc
slaveeks <chernyshev2001@icloud.com>
2023-07-21 02:39:20 +0300

    Removed model.sync()

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -40,8 +40,6 @@ export async function init(databaseConfig: DatabaseConfig): Promise<Repositories
   const noteStorage = new NoteStorage(orm);
   const userStorage = new UserStorage(orm);
 
-  await userStorage.model.sync();
-
   /**
    * Create transport instances
    */
