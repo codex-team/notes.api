9849e52d85faaef265cf36c2601d797c35adaea5
n0str <team@codex.so>
2023-07-30 12:59:19 +0300

    linter fix

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -58,11 +58,12 @@ export async function init(databaseConfig: DatabaseConfig): Promise<Repositories
   const userStorage = new UserStorage(orm);
   const userSessionStorage = new UserSessionStorage(orm);
   const editorToolsStorage = new EditorToolsStorage(orm);
-  noteStorage.model.sync()
-  noteStorage.settingsModel.sync()
-  userStorage.model.sync()
-  userSessionStorage.model.sync()
-  editorToolsStorage.model.sync()
+
+  noteStorage.model.sync();
+  noteStorage.settingsModel.sync();
+  userStorage.model.sync();
+  userSessionStorage.model.sync();
+  editorToolsStorage.model.sync();
 
   /**
    * Create transport instances
