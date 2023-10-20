4c8078c96f272e03880fc1eace58eb926144c74f
n0str <team@codex.so>
2023-10-20 03:11:18 +0300

    optimize migration run without ts compiler

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-node --loader ts-node/esm dist/repository/storage/postgres/migrations/index.js -c app-config.yaml
+node dist/repository/storage/postgres/migrations/index.js -c app-config.yaml
 node dist/index.js
