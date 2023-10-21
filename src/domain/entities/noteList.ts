bc9782972baaa4f3ce84f011ec5b464bede4c18c
Alexander Chernyaev <265195@niuitmo.ru>
2023-10-21 19:19:53 +0300

    Improved description of note list entities

--- src/domain/entities/noteList.ts
+++ src/domain/entities/noteList.ts
@@ -1,7 +1,8 @@
 import type { Note } from '@domain/entities/note.js';
 
 /**
- * Note list  entity
+ * Note list  entity.
+ * An object with the "items" property containing a list of all existing notes created by the user
  */
 export type NoteList = {
   items: Note[];
