bd21b03a28ce319f2efb9075f328b10cd94b46ca
Peter Savchenko <specc.dev@gmail.com>
2023-10-23 00:18:27 +0300

    fix: migrations

--- migrations/tenant/0009-editor-tools@rename-camelcase-to-underscore.sql
+++ migrations/tenant/0009-editor-tools@rename-camelcase-to-underscore.sql
@@ -1,24 +1,33 @@
-/*
- Rename columns in "editor_tools" table:
-*/
--- Rename column "isDefault" to "is_default" if exists
+-- "editor_tools" table:
+
+-- Rename column "isDefault" to "is_default" if "isDefault" exists and "is_default" not exists
 DO $$
 BEGIN
   IF EXISTS(SELECT *
     FROM information_schema.columns
     WHERE table_name='editor_tools' and column_name='isDefault')
   THEN
-    ALTER TABLE "public"."editor_tools" RENAME COLUMN "isDefault" TO "is_default";
+    IF NOT EXISTS(SELECT *
+      FROM information_schema.columns
+      WHERE table_name='editor_tools' and column_name='is_default')
+    THEN
+      ALTER TABLE "public"."editor_tools" RENAME COLUMN "isDefault" TO "is_default";
+    END IF;
   END IF;
 END $$;
 
--- Rename column "exportName" to "export_name" if exists
+-- Rename column "exportName" to "export_name" if "exportName" exists and "export_name" not exists
 DO $$
 BEGIN
   IF EXISTS(SELECT *
     FROM information_schema.columns
     WHERE table_name='editor_tools' and column_name='exportName')
   THEN
-    ALTER TABLE "public"."editor_tools" RENAME COLUMN "exportName" TO "export_name";
+    IF NOT EXISTS(SELECT *
+      FROM information_schema.columns
+      WHERE table_name='editor_tools' and column_name='export_name')
+    THEN
+      ALTER TABLE "public"."editor_tools" RENAME COLUMN "exportName" TO "export_name";
+    END IF;
   END IF;
 END $$;
