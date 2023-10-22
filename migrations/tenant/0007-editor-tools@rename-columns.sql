52ca84200888062024a7ea0c2026de6efaea81f2
GeekaN2 <geekan@bk.ru>
2023-10-22 04:09:11 +0300

    feat: improve migration

--- migrations/tenant/0007-editor-tools@rename-columns.sql
+++ migrations/tenant/0007-editor-tools@rename-columns.sql
@@ -1,11 +1,23 @@
-ALTER TABLE IF EXISTS public.editor_tools
-    RENAME COLUMN "name" TO "title";
+DO $$
+BEGIN
+  IF EXISTS(SELECT *
+    FROM information_schema.columns
+    WHERE table_name='editor_tools' and column_name='name' and column_name='pluginId')
+  THEN
+      ALTER TABLE IF EXISTS public.editor_tools RENAME COLUMN "name" TO "title";
+      ALTER TABLE IF EXISTS public.editor_tools RENAME COLUMN "pluginId" TO "name";
+  END IF;
+END $$;
 
-ALTER TABLE IF EXISTS public.editor_tools
-    RENAME COLUMN "pluginId" TO "name";
-
-ALTER TABLE IF EXISTS public.editor_tools
-    RENAME COLUMN "class" TO "exportName";
+DO $$
+BEGIN
+  IF EXISTS(SELECT *
+    FROM information_schema.columns
+    WHERE table_name='editor_tools' and column_name='class')
+  THEN
+      ALTER TABLE IF EXISTS public.editor_tools RENAME COLUMN "class" TO "exportName";
+  END IF;
+END $$;
 
 ALTER TABLE IF EXISTS public.editor_tools
     ADD COLUMN IF NOT EXISTS "isDefault" BOOLEAN;
