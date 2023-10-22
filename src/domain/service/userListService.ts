347879ea6ba87ede175da704cd514be898213516
paha_koha <pashagerasimik@mail.ru>
2023-10-22 13:28:41 +0300

    Fixed code style

--- src/domain/service/userListService.ts
+++ src/domain/service/userListService.ts
@@ -3,21 +3,21 @@ import type UserListRepository from '@repository/userList.repository';
 
 
 /**
- *
+ * User List service 
  */
 export default class UserListService {
   public readonly repository: UserListRepository;
 
   /**
-   *
-   * @param repository
+   * User List service constructor 
+   * @param repository - userList repository instance 
    */
   constructor(repository: UserListRepository) {
     this.repository = repository;
   }
 
   /**
-   *
+   * Get all users
    */
   public async getAllUsers(): Promise<User[]>  {
     return await this.repository.getAllUsers();
