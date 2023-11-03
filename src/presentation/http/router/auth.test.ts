c04144eacdab68b120aab70ad0c6b702cb952d92
Danya <dan.shell@bk.ru>
2023-11-03 21:51:23 +0300

    change format of checking in test with status 200

--- src/presentation/http/router/auth.test.ts
+++ src/presentation/http/router/auth.test.ts
@@ -1,46 +1,46 @@
-import { describe, test, expect } from 'vitest'
+import { describe, test, expect } from 'vitest';
 
 describe('Auth API', () => {
-    describe('POST /auth', () => {
-        test('Returns 401 when session is not valid', async () => {
-            const expectedStatus = 401
-            const refreshToken = "not-validToken";
-            const response = await global.api?.fakeRequest({
-                method: 'POST',
-                url: '/auth', // write not authorized data
-                body: { token: refreshToken }
-            })
+  describe('POST /auth', () => {
+    test('Returns 401 when session is not valid', async () => {
+      const expectedStatus = 401;
+      const refreshToken = 'not-validToken';
+      const response = await global.api?.fakeRequest({
+        method: 'POST',
+        url: '/auth', // write not authorized data
+        body: { token: refreshToken },
+      });
 
-            expect(response?.statusCode).toBe(expectedStatus)
+      expect(response?.statusCode).toBe(expectedStatus);
 
-            const body = response?.body !== undefined ? JSON.parse(response?.body) : {}
+      const body = response?.body !== undefined ? JSON.parse(response?.body) : {};
 
-            expect(body).toStrictEqual({ message: "Session is not valid" })
-        })
+      expect(body).toStrictEqual({ message: 'Session is not valid' });
+    });
 
-        test('Returns 200 when session was authorized', async () => {
-            const expectedStatus = 200
+    test('Returns 200 when session was authorized', async () => {
+      const expectedStatus = 200;
 
-            // Define the token to include in the request body
-            const refreshToken = "pv-jIqfPj1";
+      // Define the token to include in the request body
+      const refreshToken = 'pv-jIqfPj1';
 
-            const expectedAuthReply = {
-                refreshToken: "pv-jIqfPj1",
-                accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTksImlhdCI6MTY5ODc1NzQyMSwiZXhwIjoxNjk5NjU3NDIxfQ.g0PzlWpGkw4VQrMRulNrnnAFa3KxtF4buCjqxKV-wq4"
-            };
+      const expectedAuthReply = {
+        refreshToken: expect.any(String),
+        accessToken: expect.any(String),
+      };
 
-            // Include the token in the request body
-            const response = await global.api?.fakeRequest({
-                method: 'POST',
-                url: "/auth", // write authorized data
-                body: { token: refreshToken } // Include the token in the request body
-            })
+      // Include the token in the request body
+      const response = await global.api?.fakeRequest({
+        method: 'POST',
+        url: '/auth', // write authorized data
+        body: { token: refreshToken }, // Include the token in the request body
+      });
 
-            expect(response?.statusCode).toBe(expectedStatus)
+      expect(response?.statusCode).toBe(expectedStatus);
 
-            const body = response?.body !== undefined ? JSON.parse(response?.body) : {}
+      const body = response?.body !== undefined ? JSON.parse(response?.body) : {};
 
-            expect(body).toStrictEqual(expectedAuthReply)
-        })
-    })
-})
+      expect(body).toMatchObject(expectedAuthReply);
+    });
+  });
+});
