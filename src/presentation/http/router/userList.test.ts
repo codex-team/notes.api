ddf2ed89377ccaef29a78c5370abbdae271b5dd3
elizachi <lizachig2003@gmail.com>
2023-11-12 01:05:02 +0300

    Tests for userList

--- src/presentation/http/router/userList.test.ts
+++ src/presentation/http/router/userList.test.ts
@@ -1,16 +1,60 @@
-import { describe, test, expect } from 'vitest';
+import userSessions from '@tests/test-data/user-sessions.json';
+import { describe, test, expect, beforeAll } from 'vitest';
 
-describe('User list API', () => {
-  describe('GET users ', () => {
-    test('Returns list of users by pages', async () => {
+
+/**
+ * Access token that will be used for Auhorization header
+ */
+let accessToken = '';
+
+describe('UserList API', () => {
+  beforeAll(() => {
+    /**
+     * userId for authorization
+     */
+    const userId = userSessions[0]['user_id'];
+
+    accessToken = global.auth(userId);
+  });
+
+  describe('GET /users?page', () => {
+    test.each([
+      { portionSize : 3,
+        pageNumber: 1 },
+
+      { portionSize: 0,
+        pageNumber : 2 },
+    ])
+    ('Returns a userList with specified lengths with status 200', async ({ portionSize, pageNumber }) => {
       const expectedStatus = 200;
 
       const response = await global.api?.fakeRequest({
-        headers: {
-          authorization: `Bearer ${global.auth(1)}`,
-        },
         method: 'GET',
-        url: '/users/?page=1',
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+        url: `/users?page=${pageNumber}`,
+      });
+
+      expect(response?.statusCode).toBe(expectedStatus);
+
+      expect(response?.json().items).toHaveLength(portionSize);
+    });
+
+    test.each([
+      { pageNumber : 0 },
+
+      { pageNumber : 31 },
+    ])
+    ('Returns a status of 400 when the page number goes beyond the set values', async ({ pageNumber }) => {
+      const expectedStatus = 400;
+
+      const response = await global.api?.fakeRequest({
+        method: 'GET',
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+        url: `/users?page=${pageNumber}`,
       });
 
       expect(response?.statusCode).toBe(expectedStatus);
