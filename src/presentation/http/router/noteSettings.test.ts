59c45d98036b5704cac6ee3ec06e2a9cd39938e6
elizachi <lizachig2003@gmail.com>
2023-11-12 12:13:32 +0300

    Added test cases for 400 and 406 statuses

--- src/presentation/http/router/noteSettings.test.ts
+++ src/presentation/http/router/noteSettings.test.ts
@@ -6,7 +6,7 @@ describe('NoteSettings API', () => {
       const expectedStatus = 200;
       const existedNotePublicId = 'Pq1T9vc23Q';
 
-      const expectedNote = {
+      const expectedNoteSettings = {
         'customHostname': 'codex.so',
         'isPublic': true,
         'id': 2,
@@ -22,7 +22,39 @@ describe('NoteSettings API', () => {
 
       const body = response?.body !== undefined ? JSON.parse(response?.body) : {};
 
-      expect(body).toStrictEqual(expectedNote);
+      expect(body).toStrictEqual(expectedNoteSettings);
     });
   });
+
+  test('Returns 406 when the id  does not exist', async () => {
+    const expectedStatus = 406;
+    const nonexistentId = 'ishvm5qH84';
+
+    const response = await global.api?.fakeRequest({
+      method: 'GET',
+      url: `/note-settings/${nonexistentId}`,
+    });
+
+    expect(response?.statusCode).toBe(expectedStatus);
+
+    expect(response?.json()).toStrictEqual({ message: 'Note not found' });
+  });
+
+  test('Returns 400 when an id has invalid characters', async () => {
+    const expectedStatus = 400;
+    const invalidId = '+=*&*5%&&^&-';
+
+    const response = await global.api?.fakeRequest({
+      method: 'GET',
+      url: `/note-settings/${invalidId}`,
+    });
+
+    expect(response?.statusCode).toBe(expectedStatus);
+
+    expect(response?.json().message).toStrictEqual('\'/note-settings/+=*&*5%&&^&-\' is not a valid url component');
+  });
+
+  test.todo('Returns 400 when an id has incorrect length');
+
+  test.todo('Returns 403 when public access is disabled, user is not creator of the note');
 });
