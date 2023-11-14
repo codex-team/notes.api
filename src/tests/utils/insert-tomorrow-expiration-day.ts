33d01673e3a6700940705f9c9958286b5b9e3637
Danya <dan.shell@bk.ru>
2023-11-15 00:08:02 +0300

    rewrite insertUserSessions to fill dynamically updated expiration date

--- src/tests/utils/insert-tomorrow-expiration-day.ts
+++ src/tests/utils/insert-tomorrow-expiration-day.ts
@@ -1,18 +1,16 @@
-import * as fs from 'fs';
 /** create string of tomorrow  expiration day */
 export const formattedDate = (): string => {
   const tomorrow = new Date();
   const border = 19;
 
+
   tomorrow.setDate(tomorrow.getDate() + 1);
 
-  return tomorrow.toISOString().slice(0, border)
+  const newExpirationDay: string = tomorrow.toISOString().slice(0, border)
     .replace('T', ' ') + '+02';
-};
 
-export const updateRefreshTokenExpiry = (filePath: string): void => {
-  const userSessionData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-
-  userSessionData[1].refresh_token_expires_at = formattedDate();
-  fs.writeFileSync(filePath, JSON.stringify(userSessionData, null, 2), 'utf-8');
+  return newExpirationDay;
 };
+// Export the newExpirationDay variable directly
+export const newExpirationDay: string = formattedDate();
+
