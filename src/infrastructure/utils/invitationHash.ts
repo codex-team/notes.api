1bb2048c57118d2eaf6ddafb6a3fbbb20a3a9990
Ivan Skvorcov <mainpostskvorcov@gmail.com>
2023-12-05 19:10:39 +0300

    added util that generates invitation hash

---
+++ src/infrastructure/utils/invitationHash.ts
@@ -0,0 +1,11 @@
+import { nanoid } from 'nanoid';
+
+/**
+ * Create invitation hash
+ * Used to invite users to team
+ *
+ * @param length - length of invitation hash
+ */
+export function createInvitationHash(length: number = 10): string {
+  return nanoid(length);
+}
