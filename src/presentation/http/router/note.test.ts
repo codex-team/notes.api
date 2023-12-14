ce3539ed66789ca4d2d15ed93efc46d1d965e7ba
D2J3D <gridden1.student@gmail.com>
2023-12-14 17:46:57 +0300

    Added two tests for POST/note (200 and 401 status codes without passing parentId)

--- src/presentation/http/router/note.test.ts
+++ src/presentation/http/router/note.test.ts
@@ -383,6 +383,48 @@ describe('Note API', () => {
   });
 
   describe('POST /note', () => {
+
+    test('Post a new note without parentId passed', async () => {
+      const expectedStatus = 200;
+      const userId = 2;
+      const accessToken = global.auth(userId);
+
+      /** POST fakeRequest sends back a NotePublicId typeof string */
+      const post_response = await global.api?.fakeRequest({
+        method: 'POST',
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+        url: `/note`,
+        body: {},
+      });
+
+      /** Trying to fetch created note by its publicId */
+      const get_response = await global.api?.fakeRequest({
+        method: 'GET',
+        url: `/note/${post_response?.json().id}`,
+      });
+
+      expect(post_response?.statusCode).toBe(expectedStatus);
+      expect(post_response?.json().id).toBeTypeOf('string');
+      expect(post_response?.json().id.length).toBeGreaterThan(0);
+
+      expect(get_response?.statusCode).toBe(expectedStatus);
+    });
+
+    test('Returns status 401 when the user is not authorized', async () => {
+      const expectedStatus = 401;
+
+      const response = await global.api?.fakeRequest({
+        method: 'POST',
+        url: `/note`,
+        body: {},
+      });
+
+      expect(response?.statusCode).toBe(expectedStatus);
+      expect(response?.json()).toStrictEqual({ message: 'You must be authenticated to access this resource' });
+    });
+
     test('Post a new note with 200 status when the existing parentId passed', async () => {
       const expectedStatus = 200;
       const accessToken = global.auth(2);
