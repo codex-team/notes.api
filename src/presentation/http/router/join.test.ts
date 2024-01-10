e10b7e75470261b577ae7740e803817b4273dc5c
slaveeks <chernyshev2001@icloud.com>
2024-01-10 21:39:18 +0300

    refactor: removed expectedStatus and expectedResponse vars from join tests

--- src/presentation/http/router/join.test.ts
+++ src/presentation/http/router/join.test.ts
@@ -8,10 +8,6 @@ describe('Join API', () => {
       const userId = 2;
       const accessToken = global.auth(userId);
 
-      const expectedResponse = {
-        message: 'Team with user specified user and note already exists',
-      };
-
       const response = await global.api?.fakeRequest({
         method: 'POST',
         headers: {
@@ -20,19 +16,17 @@ describe('Join API', () => {
         url: `/join/${hash}`,
       });
 
-      expect(response?.statusCode).toBe(expectedStatus);
-      expect(await response?.json()).toStrictEqual(expectedResponse);
+      expect(response?.statusCode).toBe(406);
+
+      expect(await response?.json()).toStrictEqual({
+        message: 'Team with user specified user and note already exists',
+      });
     });
     test('Returns 406 when invitation hash is not valid', async () => {
-      const expectedStatus = 406;
       const hash = 'Jih23y4igf';
       const userId = 4;
       const accessToken = global.auth(userId);
 
-      const expectedResponse = {
-        message: `Note with invitation ${hash} does not exists`,
-      };
-
       const response = await global.api?.fakeRequest({
         method: 'POST',
         headers: {
@@ -41,20 +35,15 @@ describe('Join API', () => {
         url: `/join/${hash}`,
       });
 
-      expect(response?.statusCode).toBe(expectedStatus);
-      expect(await response?.json()).toStrictEqual(expectedResponse);
+      expect(response?.statusCode).toBe(406);
+
+      expect(await response?.json()).toStrictEqual({
+        message: `Note with invitation ${hash} does not exists`,
+      });
     });
     test('Returns 200 when user is added to the team', async () => {
       const userId = 1;
       const noteId = 1;
-      const expectedResponse = {
-        result: {
-          id: 2,
-          noteId,
-          userId,
-          role: 0,
-        },
-      };
       const expectedStatus = 200;
       const hash = 'Hzh2hy4igf';
       const accessToken = global.auth(userId);
@@ -67,8 +56,16 @@ describe('Join API', () => {
         url: `/join/${hash}`,
       });
 
-      expect(response?.statusCode).toBe(expectedStatus);
-      expect(await response?.json()).toStrictEqual(expectedResponse);
+      expect(response?.statusCode).toBe(200);
+
+      expect(await response?.json()).toStrictEqual({
+        result: {
+          id: 2,
+          noteId,
+          userId,
+          role: 0,
+        },
+      };);
     });
   });
 });
