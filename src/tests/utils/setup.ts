25045673d5c074cc36c760adc48ce1de09c49c42
e11sy <Egoramurin@gmail.com>
2024-02-13 17:05:27 +0300

    added new helpers and doc
    - added doc for methods
    - added insertNoteSettings method
    - added insertNoteTeam method
    - added insertNoteRelation method
    - added insertEditorTool method

--- src/tests/utils/setup.ts
+++ src/tests/utils/setup.ts
@@ -56,22 +56,61 @@ declare global {
     /**
      * Execute sql query, that creates new note in database with specified data
      *
-     * @param creatorId - id of creator user
-     * @param content - optional content of the note
+     * @param note - note object which contain all info about note (some info is optional)
      */
     insertNote: (note: {creatorId: number, content?: JSON, publicId: string}) => Promise<unknown>;
 
+    /**
+     * Execute sql query, that creates new user in database with specified data
+     *
+     * @param user - user object which contain all info about user (some info is optional)
+     *
+     * if no editorTools passed, then editor_tools would be null in database
+     */
     insertUser: (user: {email: string, name: string, editorTools?: [string]}) => Promise<unknown>;
 
+    /**
+     * Execute sql query, that creates new userSession in database with specified data
+     *
+     * @param userSession - userSession object which contain all info about userSession (some info is optional)
+     *
+     * refreshTokenExpiresAt should be given as Postgres DATE string (e.g. `CURRENT_DATE + INTERVAL '1 day'`)
+     *
+     * if no refreshTokenExpiresAt passed, then it would be `CURRENT_DATE + INTERVAL '1 day'`
+     */
     insertUserSession: (userSession: {userId: number, refreshToker: string, refreshTokenExpiresAt?: string}) => Promise<unknown>;
 
+    /**
+     * Execute sql query, that creates new noteSettings in database with specified data
+     *
+     * @param noteSettings - noteSettings object which contain all info about noteSettings (some info is optional
+     *
+     * if no custoHostname passed, then custom_hostname would be null in database
+     */
     insertNoteSetting: (noteSettings: {noteId: number, customHostname?: string, isPublic: boolean, invitationHash: string}) => Promise<unknown>;
 
-    insertEditorTool: () => Promise<unknown>;
+    /**
+     * Execute sql query, that creates new noteTeam in database with specified data
+     *
+     * @param noteTeam - noteTam object which contain all info about noteTeam
+     */
+    insertNoteTeam: (noteTeam: {userId: number, noteId: number, role: number}) => Promise<unknown>;
 
-    insertNoteRelation: () => Promise<unknown>;
+    /**
+     * Execute sql query, that creates new noteRelation in database with specified data
+     *
+     * @param noteRelation object which contain all info about noteRelation
+     */
+    insertNoteRelation: (noteRelation: {noteId: number, parentId: number}) => Promise<unknown>;
 
-    insertNoteTeam: () => Promise<unknown>;
+    /**
+     * Execute sql query, that creates new editorTool in database with specified data
+     *
+     * @param editorTool object which contain all info about editorTool (some info is optional)
+     *
+     * if no isDefault passed, then is_default would be 0 in database
+     */
+    insertEditorTool: (editorTool: {name: string, title: string, exportName: string, source: JSON, isDefault?: boolean}) => Promise<unknown>;
   };
 }
 
@@ -111,7 +150,9 @@ beforeAll(async () => {
     },
 
     insertNote: async (note: {creatorId: number, content?: JSON, publicId: string}) => {
-      return await orm.connection.query(`INSERT INTO public.notes ("content", "creator_id", "created_at", "updated_at", "public_id") VALUES ('${note.content}', ${note.creatorId}, CURRENT_DATE, CURRENT_DATE, '${note.publicId}')`);
+      const content = note.content ?? {};
+
+      return await orm.connection.query(`INSERT INTO public.notes ("content", "creator_id", "created_at", "updated_at", "public_id") VALUES ('${content}', ${note.creatorId}, CURRENT_DATE, CURRENT_DATE, '${note.publicId}')`);
     },
 
     insertUser: async (user: {email: string, name: string, editorTools?: [string]}) => {
@@ -123,7 +164,7 @@ beforeAll(async () => {
 
     insertUserSession: async (userSession: {userId: number, refreshToker: string, refreshTokenExpiresAt?: string}) => {
       // refreshTokerExpiresAt is optional so it would be 'CURRENT_DATE + INTERVAL(1 DAY)' by default
-      const refreshTokerExpiresAt = userSession.refreshTokenExpiresAt ?? 'CURRENT_DATE + INTERVAL(1 DAY)';
+      const refreshTokerExpiresAt = userSession.refreshTokenExpiresAt ?? `CURRENT_DATE + INTERVAL '1 day')`;
 
       return await orm.connection.query(`INSERT INTO public.user_sessions ("user_id", "refresh_token", "refresh_toker_expires_at") VALUES (${userSession.userId}, '${userSession.refreshToker}, '${refreshTokerExpiresAt}')`);
     },
@@ -132,19 +173,22 @@ beforeAll(async () => {
       // custom hostname could be `undefined` because it is optional, but db needs `null` property in this case
       const customHostname = noteSettings.customHostname ?? null;
 
-      return await orm.connection.query(`INSERT INTO public.note_settings ("note_id", "custom_hostname", "is_public", "invitation_hash") VALUES (${noteSettings.noteId}, '${customHostname}')`);
+      return await orm.connection.query(`INSERT INTO public.note_settings ("note_id", "custom_hostname", "is_public", "invitation_hash") VALUES (${noteSettings.noteId}, '${customHostname}', ${noteSettings.isPublic}, '${noteSettings.invitationHash}')`);
     },
 
-    insertNoteTeam: async () => {
-      return await orm.connection.query(``);
+    insertNoteTeam: async (noteTeam: {userId: number, noteId: number, role: number}) => {
+      return await orm.connection.query(`INSERT INTO public.note_teams ('user_id', 'note_id', 'role') VALUES (${noteTeam.userId}, ${noteTeam.noteId}, ${noteTeam.role})`);
     },
 
-    insertNoteRelation: async () => {
-      return await orm.connection.query(``);
+    insertNoteRelation: async (noteRelation: {noteId: number, parentId: number}) => {
+      return await orm.connection.query(`INSERT INTO public.note_relations ('note_id', 'parent_id') VALUES (${noteRelation.noteId}, ${noteRelation.parentId})`);
     },
 
-    insertEditorTool: async () => {
-      return await orm.connection.query(``);
+    insertEditorTool: async (editorTool: {name: string, title: string, exportName: string, source: JSON, isDefault?: boolean }) => {
+      // custom hostname could be `undefined` because it is optional, but db needs `null` property in this case
+      const isDefault = editorTool.isDefault ?? null;
+
+      return await orm.connection.query(`INSERT INTO public.editor_tools ('name', 'title', 'export_name', 'sourse', 'is_default') VALUES ('${editorTool.name}', '${editorTool.title}', '${editorTool.exportName}', '${editorTool.exportName}', ${isDefault}')`);
     },
   };
 }, TIMEOUT);
