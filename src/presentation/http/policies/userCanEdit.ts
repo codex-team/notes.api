aaecb6ddbd6180735c2323dd6d4d0087d5b6844c
GoldenJaden <yaroslav-gusev-00@mail.ru>
2024-02-14 14:43:52 +0300

    Add policy to check if user can edit the note

---
+++ src/presentation/http/policies/userCanEdit.ts
@@ -0,0 +1,35 @@
+import type { FastifyReply, FastifyRequest } from 'fastify';
+import { isEmpty } from '@infrastructure/utils/empty.js';
+
+/**
+ * Policy to check whether a user has permission to edit the note
+ *
+ * @param request - Fastify request object
+ * @param reply - Fastify reply object
+ */
+export default async function userCanEdit(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+  const { userId } = request;
+
+  /**
+   * If note or memberRole is not resolved, we can't check permissions
+   */
+  if (isEmpty(request.note) || isEmpty(request.memberRole)) {
+    return await reply.notAcceptable('Note not found');
+  };
+
+  if (isEmpty(request.memberRole)) {
+    return await reply.notAcceptable('Team member not found');
+  }
+
+  const { creatorId } = request.note;
+  const { memberRole } = request;
+
+  /**
+   * If user is not a creator of the note and
+   * user has a Reader Role (0) or is not in team at all,
+   * he doesn't have permission to edit the note
+   */
+  if (creatorId !== userId && memberRole !== 1) {
+    return await reply.forbidden();
+  }
+}
