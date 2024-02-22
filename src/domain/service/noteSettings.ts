ea90ff5f91e9f1c97cfc0d938d1d737207ad6bd2
Ivan Skvorcov <mainpostskvorcov@gmail.com>
2024-02-23 00:36:21 +0300

    small fixes

--- src/domain/service/noteSettings.ts
+++ src/domain/service/noteSettings.ts
@@ -8,7 +8,7 @@ import { MemberRole } from '@domain/entities/team.js';
 import type User from '@domain/entities/user.js';
 import { createInvitationHash } from '@infrastructure/utils/invitationHash.js';
 import { DomainError } from '@domain/entities/DomainError.js';
-import type UserRepository from '@repository/user.repository';
+import type NoteRepository from '@repository/note.repository';
 
 /**
  * Service responsible for Note Settings
@@ -21,19 +21,19 @@ export default class NoteSettingsService {
 
   private readonly teamRepository: TeamRepository;
 
-  private readonly userRepository: UserRepository;
+  private readonly noteRepository: NoteRepository;
 
   /**
    * Note Settings service constructor
    *
    * @param noteSettingsRepository - note settings repository
    * @param teamRepository - team repository
-   * @param userRepository - user repository
+   * @param noteRepository - note repository
    */
-  constructor(noteSettingsRepository: NoteSettingsRepository, teamRepository: TeamRepository, userRepository: UserRepository) {
+  constructor(noteSettingsRepository: NoteSettingsRepository, teamRepository: TeamRepository, noteRepository: NoteRepository) {
     this.noteSettingsRepository = noteSettingsRepository;
     this.teamRepository = teamRepository;
-    this.userRepository = userRepository;
+    this.noteRepository = noteRepository;
   }
 
   /**
