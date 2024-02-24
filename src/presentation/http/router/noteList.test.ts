3de42bc2195b65df387257419ac0c265dff62e49
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2024-02-24 13:40:59 +0300

    Refactor noteList tests

--- src/presentation/http/router/noteList.test.ts
+++ src/presentation/http/router/noteList.test.ts
@@ -1,26 +1,37 @@
-import userSessions from '@tests/test-data/user-sessions.json';
-import { describe, test, expect, beforeAll } from 'vitest';
-
-
-/**
- * Access token that will be used for Auhorization header
- */
-let accessToken = '';
+import { describe, test, expect, beforeEach } from 'vitest';
 
 describe('NoteList API', () => {
-  beforeAll(() => {
+  beforeEach(async () => {
     /**
-     * userId for authorization
+     * Truncate all tables, which are needed
+     * Restart autoincrement sequences for data to start with id 1
+     *
+     * TODO get rid of restarting database data in tests (move to beforeEach)
      */
-    const userId = userSessions[0]['user_id'];
-
-    accessToken = global.auth(userId);
+    await global.db.truncateTables();
   });
 
   describe('GET /notes?page', () => {
     test('Returns noteList with specified length (not for last page)', async () => {
       const portionSize = 30;
       const pageNumber = 1;
+
+      /** Create test user */
+      const user = await global.db.insertUser({
+        email: 'test@codexmail.com',
+        name: 'CodeX',
+      });
+
+      /** Authorize user */
+      const accessToken = global.auth(user.id);
+
+      /** Create test notes */
+      for (let i = 0; i < 49; i++) {
+        await global.db.insertNote({
+          creatorId: user.id,
+          publicId: 'TJmEb89e0l',
+        });
+      }
 
       const response = await global.api?.fakeRequest({
         method: 'GET',
@@ -41,6 +52,23 @@ describe('NoteList API', () => {
       const portionSize = 19;
       const pageNumber = 2;
 
+      /** Create test user */
+      const user = await global.db.insertUser({
+        email: 'test@codexmail.com',
+        name: 'CodeX',
+      });
+
+      /** Authorize user */
+      const accessToken = global.auth(user.id);
+
+      /** Create test notes */
+      for (let i = 0; i < 49; i++) {
+        await global.db.insertNote({
+          creatorId: user.id,
+          publicId: 'TJmEb89e0l',
+        });
+      }
+
       const response = await global.api?.fakeRequest({
         method: 'GET',
         headers: {
@@ -58,6 +86,15 @@ describe('NoteList API', () => {
 
     test('Returns noteList with no items if it has no notes', async () => {
       const pageNumber = 3;
+
+      /** Create test user */
+      const user = await global.db.insertUser({
+        email: 'test@codexmail.com',
+        name: 'CodeX',
+      });
+
+      /** Authorize user */
+      const accessToken = global.auth(user.id);
 
       const response = await global.api?.fakeRequest({
         method: 'GET',
@@ -78,6 +115,15 @@ describe('NoteList API', () => {
     test('Returns 400 when page < 0', async () => {
       const pageNumber = 0;
 
+      /** Create test user */
+      const user = await global.db.insertUser({
+        email: 'test@codexmail.com',
+        name: 'CodeX',
+      });
+
+      /** Authorize user */
+      const accessToken = global.auth(user.id);
+
       const response = await global.api?.fakeRequest({
         method: 'GET',
         headers: {
@@ -89,8 +135,17 @@ describe('NoteList API', () => {
       expect(response?.statusCode).toBe(400);
     });
 
-    test('Returns 400 when page is too large (maximum page numbrer is 30 by default)', async () => {
+    test.only('Returns 400 when page is too large (maximum page number is 30 by default)', async () => {
       const pageNumber = 31;
+
+      /** Create test user */
+      const user = await global.db.insertUser({
+        email: 'test@codexmail.com',
+        name: 'CodeX',
+      });
+
+      /** Authorize user */
+      const accessToken = global.auth(user.id);
 
       const response = await global.api?.fakeRequest({
         method: 'GET',
