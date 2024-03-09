7bbb76ddab4847edc5150b292f84b34123e17395
slaveeks <chernyshev2001@icloud.com>
2024-03-09 17:31:04 +0300

    refactor: add check if column extension not exists in migration

--- migrations/tenant/0026-files@rename-extension-to-mimetype.sql
+++ migrations/tenant/0026-files@rename-extension-to-mimetype.sql
@@ -5,6 +5,11 @@ BEGIN
     FROM   information_schema.columns
     WHERE  table_name = 'files'
     AND    column_name = 'extension'
+  ) AND NOT EXISTS (
+    SELECT 1
+    FROM   information_schema.columns
+    WHERE  table_name = 'files'
+    AND    column_name = 'mimetype'
   )
   THEN
     ALTER TABLE public.files RENAME COLUMN extension TO mimetype;
