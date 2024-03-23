22d1c3204f25d282e317287ece3c059202e32b21
e11sy <Egoramurin@gmail.com>
2024-03-23 16:13:15 +0300

    added index on user_id field
    - moved CONSTRAINTS addition to table creation
    - added user_id index

--- migrations/tenant/0027-note-views@add-note-views-table.sql
+++ migrations/tenant/0027-note-views@add-note-views-table.sql
@@ -3,33 +3,17 @@
 --
 
 CREATE TABLE IF NOT EXISTS public.note_views (
-    id integer NOT NULL,
-    user_id integer NOT NULL,
-    note_id integer NOT NULL,
-    "interaction_time" timestamp with time zone NOT NULL
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES public.users(id)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    note_id integer NOT NULL REFERENCES public.notes(id)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    "vizited_at" timestamp with time zone NOT NULL
 );
 
 --
--- Name: note_views note_views_pkey; Type: PK CONSTRAINT; Schema: public; Owner: codex
+-- Name note_views note_views_user_id_idx; Type: INDEX; Schema: public; Owner: codex 
 --
-ALTER TABLE public.note_views DROP CONSTRAINT IF EXISTS note_views_pkey;
-ALTER TABLE public.note_views
-    ADD CONSTRAINT note_view_pkey PRIMARY KEY (id);
-
---
--- Name: note_view user_id_fkey; Type: FK CONSTRAINT; Shema: public; Owner: codex
--- 
-ALTER TABLE public.note_views DROP CONSTRAINT IF EXISTS user_id_fkey;
-ALTER TABLE public.note_views 
-    ADD CONSTRAINT user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
-
---
--- Name note_view note_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: codex
---
-ALTER TABLE public.note_views DROP CONSTRAINT IF EXISTS note_id_fkey;
-ALTER TABLE public.note_views
-    ADD CONSTRAINT note_id_fkey FOREIGN KEY (note_id) REFERENCES public.notes(id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+CREATE UNIQUE INDEX note_views_user_id_idx ON public.note_views (user_id);
 
 ALTER TABLE public.note_views OWNER TO codex;
