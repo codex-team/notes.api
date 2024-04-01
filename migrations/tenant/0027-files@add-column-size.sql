fedcc196fbae64a51c0e98d47980853e555d2b48
slaveeks <chernyshev2001@icloud.com>
2024-04-01 21:42:40 +0300

    feat: add location column to files, removed note_id column

--- migrations/tenant/0027-files@add-column-size.sql
+++ migrations/tenant/0027-files@add-column-size.sql
@@ -22,15 +22,25 @@ BEGIN
 -- Make identifier default value incrementing
   ALTER TABLE ONLY public.files ALTER COLUMN id SET DEFAULT nextval('public.files_id_seq'::regclass);
 
-  -- IF EXISTS (
-  --   SELECT 1
-  --   FROM information_schema.columns
-  --   WHERE table_name = 'files'
-  --   AND column_name = 'type'
-  --   AND data_type = 'character varying'
-  --   ) THEN
-  --       ALTER TABLE files
-  --       ALTER COLUMN type TYPE INTEGER
-  --       USING type::INTEGER;
-  -- END IF;
+  IF EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'files'
+        AND column_name = 'note_id'
+    ) THEN
+        -- Drop the column if it exists
+        ALTER TABLE files
+        DROP COLUMN note_id;
+  END IF;
+
+  IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'files'
+        AND column_name = 'location'
+    ) THEN
+        -- Add the new column with jsonb type
+        ALTER TABLE files
+        ADD COLUMN location JSONB;
+  END IF;
 END $$;
