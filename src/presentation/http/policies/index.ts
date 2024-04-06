8e832f3644635189d776e81f3e11742767d32284
slaveeks <chernyshev2001@icloud.com>
2024-04-06 17:14:04 +0300

    refactor: removed redundant policies

--- src/presentation/http/policies/index.ts
+++ src/presentation/http/policies/index.ts
@@ -1,13 +1,9 @@
 import authRequired from './authRequired.js';
 import notePublicOrUserInTeam from './notePublicOrUserInTeam.js';
 import userCanEdit from './userCanEdit.js';
-import userCanReadFileData from './userCanReadFileData.js';
-import userCanUploadFile from './userCanUpload.js';
 
 export default {
   authRequired,
   notePublicOrUserInTeam,
   userCanEdit,
-  userCanReadFileData,
-  userCanUploadFile,
 };
