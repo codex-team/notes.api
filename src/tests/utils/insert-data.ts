a59301868665b35268ecf7f6de22cf93ad088b73
elizachi <chigvintseva.elizaveta@gmail.com>
2024-04-27 17:50:15 +0300

    Delete notes-relations.json

--- src/tests/utils/insert-data.ts
+++ src/tests/utils/insert-data.ts
@@ -1,5 +1,4 @@
 import type SequelizeOrm from '@repository/storage/postgres/orm/index.js';
-import noteRelations from '../test-data/notes-relations.json';
 import users from '../test-data/users.json';
 import userSessions from '../test-data/user-sessions.json';
 import notes from '../test-data/notes.json';
@@ -47,19 +46,6 @@ async function insertNoteSettings(db: SequelizeOrm): Promise<void> {
     await db.connection.query(`INSERT INTO public.note_settings (id, "note_id", "custom_hostname", "is_public", "invitation_hash") VALUES (${noteSetting.id}, '${noteSetting.note_id}', '${noteSetting.custom_hostname}', ${noteSetting.is_public}, '${noteSetting.invitation_hash}')`);
   }
 }
-
-/**
- * Fills in the database with notes relationship data
- *
- * @param db - SequelizeOrm instance
- */
-async function insertNoteRelatons(db: SequelizeOrm): Promise<void> {
-  for (const noteRelation of noteRelations) {
-    await db.connection.query(`INSERT INTO public.note_relations (id, "note_id", "parent_id") VALUES (${noteRelation.id}, '${noteRelation.note_id}', '${noteRelation.parent_id}')`);
-  }
-}
-
-
 /**
  * Fills in the database with test data
  *
@@ -70,6 +56,5 @@ export async function insertData(db: SequelizeOrm): Promise<void> {
   await insertUserSessions(db);
   await insertNotes(db);
   await insertNoteSettings(db);
-  await insertNoteRelatons(db);
 }
 
