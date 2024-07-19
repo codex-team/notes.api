39aa1768cf9bdd2f2594024b3b63fc52b9a07e85
e11sy <Egoramurin@gmail.com>
2024-07-19 20:11:26 +0300

    migration for note_history table

---
+++ migrations/tenant/0032-note-history@add-note-history-table.sql
@@ -0,0 +1,29 @@
+--
+-- Name: note-history; Type: TABLE; Schema: public; Owner: codex
+--
+
+CREATE TABLE IF NOT EXISTS public.note_history (
+  id integer NOT NULL,
+  note_id integer NOT NULL,
+  user_id integer,
+  updated_at TIMESTAMP NOT NULL,
+  content json
+);
+
+--
+-- Name: note_history note_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: codex
+--
+ALTER TABLE public.note_history DROP CONSTRAINT IF EXISTS note_id_fkey;
+ALTER TABLE public.note_history
+  ADD CONSTRAINT note_id_fkey FOREIGN KEY (note_id) REFERENCES public.notes(id)
+  ON UPDATE CASCADE ON DELETE CASCADE;
+
+--
+-- Name: note_history user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: codex
+--
+ALTER TABLE public.note_history DROP CONSTRAINT IF EXISTS user_id_fkey;
+ALTER TABLE public.note_history
+  ADD CONSTRAINT user_id_fkey FOREIGN KEY (note_id) REFERENCES public.users(id)
+  ON UPDATE CASCADE ON DELETE CASCADE;
+
+CREATE UNIQUE INDEX note_history_note_id_idx ON public.note_history (note_id);
